d5b5a8275513a2e686af019cae93422701adc183
Md Azim <142779664+Azim002@users.noreply.github.com>
2024-03-09 09:31:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+      document.getElementById('bulb').src='images/bulb-on.png';
+  }
+  function bulb_off(){
+      document.getElementById('bulb').src='images/bulb-off.png';
+  }
